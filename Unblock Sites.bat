587c71f7af2d80df8ed5dcd417ca438a67bbe62b
kaz <deadconvicess@gmail.com>
2025-06-11 17:26:32 -0700

    Delete Unblock Sites.bat

--- Unblock Sites.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-takeown /f %SystemRoot%\System32\drivers\etc\hosts
-icacls %SystemRoot%\System32\drivers\etc\hosts /grant %username%:F
-del %SystemRoot%\System32\drivers\etc\hosts
-echo 127.0.0.1 localhost > %SystemRoot%\System32\drivers\etc\hosts
-echo Sites are unblocked
-pause
